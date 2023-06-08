1a99d232a2f7c82fcd4048aa5c20be034cb7a755
Nikola <nikoladjuraskovic96@gmail.com>
2023-06-08 21:30:40 +0200

    added datetime to base

--- B8KnjigaUtisaka/pocetna.aspx.cs
+++ B8KnjigaUtisaka/pocetna.aspx.cs
@@ -10,7 +10,7 @@ namespace B8KnjigaUtisaka
 {
     public partial class pocetna : System.Web.UI.Page
     {
-        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-Q7KL7JB;Initial Catalog=b8;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-Q7KL7JB;Initial Catalog=b8v2;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         SqlCommand cmd;
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -24,7 +24,7 @@ namespace B8KnjigaUtisaka
                 try
                 {
                     con.Open();
-                    string upit = "INSERT INTO Utisak(Ime,Email,Komentar) VALUES('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "')";
+                    string upit = "INSERT INTO Utisak(Ime,Email,Komentar, Datum) VALUES('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "', GETDATE())";
                     cmd = new SqlCommand(upit, con);
                     cmd.ExecuteNonQuery();
                 }
